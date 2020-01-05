0d824eafde8f1a5acb1d163147e0908b894405e1
EricSSartorius <EricSSartorius@gmail.com>
2020-01-05 15:39:11 -0700

    Comment out shopify

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,29 +16,13 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-layout`,
-    {
-      resolve: `gatsby-source-shopify`,
-      options: {
-        // The domain name of your Shopify shop. This is required.
-        // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
-        // 'gatsby-source-shopify-test-shop.myshopify.com'.
-        shopName: process.env.SHOP_NAME,
-
-        // An API access token to your Shopify shop. This is required.
-        // You can generate an access token in the "Manage private apps" section
-        // of your shop's Apps settings. In the Storefront API section, be sure
-        // to select "Allow this app to access your storefront data using the
-        // Storefront API".
-        // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
-        accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
-
-        // Set verbose to true to display a verbose output on `npm run develop`
-        // or `npm run build`. This prints which nodes are being fetched and how
-        // much time was required to fetch and process the data.
-        // Defaults to true.
-        verbose: true,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-shopify`,
+    //   options: {
+    //     shopName: process.env.SHOP_NAME,
+    //     accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -51,8 +35,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/templates/product.js
+++ src/templates/product.js
@@ -1,45 +1,45 @@
-// import React from "react"
-// import { graphql } from "gatsby"
-// import styled from "styled-components"
-// // import SEO from "~/components/seo"
-// // import ProductForm from "~/components/ProductForm"
-// // import {
-// //   Img,
-// //   Container,
-// //   TwoColumnGrid,
-// //   GridLeft,
-// //   GridRight,
-// // } from "~/utils/styles"
+import React from "react"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+// import SEO from "~/components/seo"
+// import ProductForm from "~/components/ProductForm"
+// import {
+//   Img,
+//   Container,
+//   TwoColumnGrid,
+//   GridLeft,
+//   GridRight,
+// } from "~/utils/styles"
 
-// const ProductPage = ({ data }) => {
-//   console.log("DATA", data)
-//   return (
-//     <>
-//       {/* <SEO title={product.title} description={product.description} /> */}
-//       <h1>PRODUCT PAGE</h1>
-//       {/* <Container>
-//         <TwoColumnGrid>
-//           <GridLeft>
-//             {product.images.map(image => (
-//               <Img
-//                 fluid={image.localFile.childImageSharp.fluid}
-//                 key={image.id}
-//                 alt={product.title}
-//               />
-//             ))}
-//           </GridLeft>
-//           <GridRight>
-//             <ProductTitle>{product.title}</ProductTitle>
-//             <ProductDescription
-//               dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
-//             />
-//             <ProductForm product={product} />
-//           </GridRight>
-//         </TwoColumnGrid>
-//       </Container> */}
-//     </>
-//   )
-// }
+const ProductPage = ({ data }) => {
+  // console.log("DATA", data)
+  return (
+    <>
+      {/* <SEO title={product.title} description={product.description} /> */}
+      <h1>PRODUCT PAGE</h1>
+      {/* <Container>
+        <TwoColumnGrid>
+          <GridLeft>
+            {product.images.map(image => (
+              <Img
+                fluid={image.localFile.childImageSharp.fluid}
+                key={image.id}
+                alt={product.title}
+              />
+            ))}
+          </GridLeft>
+          <GridRight>
+            <ProductTitle>{product.title}</ProductTitle>
+            <ProductDescription
+              dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+            />
+            <ProductForm product={product} />
+          </GridRight>
+        </TwoColumnGrid>
+      </Container> */}
+    </>
+  )
+}
 
 // export const query = graphql`
 //   query($handle: String!) {
@@ -92,20 +92,20 @@
 //   }
 // `
 
-// export default ProductPage
+export default ProductPage
 
-// export const ProductTitle = styled.h1`
-//   font-size: 2.25rem;
-//   margin-bottom: 15px;
-//   word-wrap: break-word;
-//   font-family: "Helvetica", "Helvetica", sans-serif;
-//   font-weight: 400;
-//   margin: 0 0 0.5rem;
-//   line-height: 1.4;
-// `
+export const ProductTitle = styled.h1`
+  font-size: 2.25rem;
+  margin-bottom: 15px;
+  word-wrap: break-word;
+  font-family: "Helvetica", "Helvetica", sans-serif;
+  font-weight: 400;
+  margin: 0 0 0.5rem;
+  line-height: 1.4;
+`
 
-// export const ProductDescription = styled.div`
-//   margin-top: 40px;
-//   font-family: "Helvetica", "Helvetica", sans-serif;
-//   font-weight: 300;
-// `
+export const ProductDescription = styled.div`
+  margin-top: 40px;
+  font-family: "Helvetica", "Helvetica", sans-serif;
+  font-weight: 300;
+`
